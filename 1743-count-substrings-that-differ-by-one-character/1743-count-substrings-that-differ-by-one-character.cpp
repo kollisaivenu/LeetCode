2ffc6e6c337474a36ee5cb2ff334c569e840181e
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-03-20 10:46:52 -0600

    Time: 0 ms (100%), Space: 8.1 MB (99.6%) - LeetHub

--- 1743-count-substrings-that-differ-by-one-character/1743-count-substrings-that-differ-by-one-character.cpp
+++ 1743-count-substrings-that-differ-by-one-character/1743-count-substrings-that-differ-by-one-character.cpp
@@ -4,30 +4,30 @@ public:
         int ans = 0;
         for(int i=s.length()-1;i>=0;i--){
             for(int j=t.length()-1;j>=0;j--){
-                int curr_ans = 0;
+                int currAns = 0;
                 if(s[i] != t[j]){
-                    int left_subarrays = 0;
-                    int go_left_i = i-1;
-                    int go_left_j = j-1;
+                    int leftSubarrays = 0;
+                    int goLeftI = i-1;
+                    int goLeftJ = j-1;
 
-                    while(go_left_i >=0 && go_left_j >= 0 && s[go_left_i] == t[go_left_j]){
-                        left_subarrays++;
-                        go_left_i--;
-                        go_left_j--;
+                    while(goLeftI >=0 && goLeftJ >= 0 && s[goLeftI] == t[goLeftJ]){
+                        leftSubarrays++;
+                        goLeftI--;
+                        goLeftJ--;
                     }
 
-                    int right_subarrays = 0;
-                    int go_right_i = i+1;
-                    int go_right_j = j+1;
+                    int rightSubarrays = 0;
+                    int goRightI = i+1;
+                    int goRightJ = j+1;
 
-                    while(go_right_i < s.length() && go_right_j < t.length() && s[go_right_i] == t[go_right_j]){
-                        right_subarrays++;
-                        go_right_i++;
-                        go_right_j++;
+                    while(goRightI < s.length() && goRightJ < t.length() && s[goRightI] == t[goRightJ]){
+                        rightSubarrays++;
+                        goRightI++;
+                        goRightJ++;
                     }
-                    curr_ans += (1 + left_subarrays)*(1 + right_subarrays);
+                    currAns += (1 + leftSubarrays)*(1 + rightSubarrays);
                 }
-                ans += curr_ans;
+                ans += currAns;
 
             }
         }
