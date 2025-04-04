4b356aeea1da1ca738f3029cd03834127020b0fa
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-04-03 18:49:53 -0600

    Time: 7 ms (8.22%), Space: 25.2 MB (5.48%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        queue<TreeNode*>q;
+        vector<vector<TreeNode*>>levels;
+        unordered_map<TreeNode*, TreeNode*>parentOf;
+        parentOf[root] = NULL;
+        q.push(root);
+
+        while(!q.empty()){
+            q.push(NULL);
+            vector<TreeNode*>level;
+            while(q.front() != NULL){
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node);
+                if(node->left){
+                    parentOf[node->left] = node;
+                    q.push(node->left);
+                }
+
+                if(node->right){
+                    parentOf[node->right] = node;
+                    q.push(node->right);
+                }
+            }
+            q.pop();
+            levels.push_back(level);
+        }
+        set<TreeNode*>parents;
+        // for(int i=0;i<levels[levels.size()-1].size();i++){
+        //     parents.insert(levels[levels.size()-1][i]);
+        // }
+
+        for(TreeNode* node: levels[levels.size()-1]){
+            parents.insert(node);
+        }
+
+        while(parents.size() != 1){
+            set<TreeNode*>tempSet;
+
+            for (TreeNode* node : parents) {
+                tempSet.insert(parentOf[node]);
+            }
+            parents = tempSet;
+        }
+        return *parents.begin();
+    }
+
+};
