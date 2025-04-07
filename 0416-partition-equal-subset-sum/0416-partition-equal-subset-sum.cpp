34da294aafa7d6de4e3837fc60b1687782ee97e6
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-04-06 19:37:41 -0600

    Time: 443 ms (5%), Space: 19.9 MB (56.89%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int totalSum = 0;
+        for(int i=0;i<nums.size();i++){
+            totalSum += nums[i];
+        }
+        if(totalSum%2 == 1){
+            return false;
+        }
+        vector<vector<bool>>dp(nums.size()+1, vector<bool>(totalSum+1, false));
+        dp[nums.size()][totalSum/2] = true;
+        
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int j=totalSum;j>=0;j--){
+                if(j+nums[i]<=totalSum){
+                    dp[i][j] = dp[i][j] || dp[i+1][j+nums[i]];
+                }
+                dp[i][j] = dp[i][j] || dp[i+1][j]; 
+            }
+        }
+        return dp[0][0];
+
+    }
+
+    bool canEqualSumPartition(vector<int>&nums, int partitionSum, int currIndex, int totalSum){
+        if(currIndex == nums.size()){
+            if(partitionSum*2 == totalSum){
+                return true;
+            }
+            return false;
+        }
+        return canEqualSumPartition(nums, partitionSum+nums[currIndex], currIndex+1, totalSum) || canEqualSumPartition(nums, partitionSum, currIndex+1, totalSum);
+    }
+};
