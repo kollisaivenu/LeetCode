ec685001822526f584beb3bf4c9ddc75a87f66e4
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-04-12 23:42:52 -0600

    Time: 0 ms (100%), Space: 8 MB (34.93%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -3,19 +3,20 @@ public:
     int countGoodNumbers(long long n) {
         long long ans;
         if(n%2 == 0){
-            ans = binpow(5, n/2, 1000000007)*binpow(4, n/2, 1000000007);
+            ans = binpow(5, n/2)*binpow(4, n/2);
         } else {
-            ans = binpow(5, n/2, 1000000007)*binpow(4, n/2, 1000000007)*5;
+            ans = binpow(5, n/2)*binpow(4, n/2)*5;
         }
         return ans%1000000007;  
     }
-    long long binpow(long long a, long long b, int mod) {
+    long long binpow(long long a, long long b) {
+        int mod = 1e9+7;
         a = a%mod;
         if (b == 0){
             return 1;
         }
 
-        long long res = binpow(a, b / 2, mod)%mod;
+        long long res = binpow(a, b / 2)%mod;
         if (b % 2){
             return (res * res * a)%mod;
         }else{
