59f511a7392474176b2cf3d1305332f37cc0f8d2
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-04-30 19:13:11 -0600

    Time: 95 ms (25.05%), Space: 55.6 MB (7.97%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        vector<int>prefixSum(nums.size(), 0);
+        prefixSum[0] = nums[0];
+        map<int, int>freqOfSum;
+        freqOfSum[0]++;
+
+        for(int i=1;i<nums.size();i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+
+        for(int i=0;i<prefixSum.size();i++){
+            count += freqOfSum[prefixSum[i]-k];
+            freqOfSum[prefixSum[i]]++;
+        }
+
+        return count;
+    }
+};
