eb45a3fc533bf9701e5f20e1c8f7d594ae6cb8f5
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2025-06-13 20:59:58 -0600

    Time: 0 ms (100%), Space: 8.2 MB (37.74%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        bool mapped = false;
+        char orig;
+
+        string largestNumInStr = to_string(num);
+
+        for(int i=0;i<largestNumInStr.length();i++){
+            if(mapped == false && largestNumInStr[i] != '9'){
+                orig = largestNumInStr[i];
+                largestNumInStr[i] = '9';
+                mapped = true;
+            } else if(mapped && largestNumInStr[i] == orig){
+                largestNumInStr[i] = '9';
+            }
+        }
+
+        int largestNum = stoi(largestNumInStr);
+
+        mapped = false;
+        string smallestNumInStr = to_string(num);
+
+        for(int i=0;i<smallestNumInStr.length();i++){
+            if(mapped == false && smallestNumInStr[i] != '0'){
+                orig = smallestNumInStr[i];
+                smallestNumInStr[i] = '0';
+                mapped = true;
+            } else if(mapped && smallestNumInStr[i] == orig){
+                smallestNumInStr[i] = '0';
+            }
+        }
+
+        //cout<<smallestNumInStr<<"\n";
+        
+        int smallestNum = stoi(smallestNumInStr);
+        //cout<<largestNum<<" "<<smallestNum<<"\n";
+
+        return largestNum - smallestNum;
+    }
+};
