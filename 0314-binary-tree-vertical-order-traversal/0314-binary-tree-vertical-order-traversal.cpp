b654078e619a031715ec586b7b39015f1275f69b
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2026-01-01 00:29:59 -0700

    Time: 0 ms (100%), Space: 15.7 MB (53.92%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
@@ -17,16 +17,16 @@ public:
         if(root == NULL) {
             return ans;
         }
-        
+
         queue<pair<TreeNode*, int>>q;
-        map<int, vector<int>>cols;
+        map<int, vector<int>>colMap;
 
         q.push(make_pair(root, 0));
 
         while(!q.empty()) {
             pair p = q.front();
             q.pop();
-            cols[p.second].push_back(p.first->val);
+            colMap[p.second].push_back(p.first->val);
 
             if(p.first->left) {
                 q.push(make_pair(p.first->left, p.second-1));
@@ -38,7 +38,7 @@ public:
 
         }
         
-        for(auto itr = cols.begin();itr!=cols.end();itr++) {
+        for(auto itr = colMap.begin();itr!=colMap.end();itr++) {
             vector<int>col;
             for(int j=0;j<itr->second.size();j++){
                 col.push_back(itr->second[j]);
