533af22bd0e51570aef9d42f596d717fe733efa8
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2026-01-03 18:17:49 -0700

    Time: 72 ms (28.82%), Space: 16.3 MB (48.28%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            int div = check(nums[i]);
+
+            if(div > 0) {
+                cout<<nums[i]<<"\n";
+                ans = ans + 1 + nums[i] + div + nums[i]/div;
+            }
+        }
+
+        return ans;
+        
+    }
+
+    int check(int num) {
+        int div = 0;
+        int count = 2;
+
+        for(int i=2;i<=(int)sqrt(num);i++) {
+            if(num%i == 0) {
+                if(i == num/i) {
+                    count++;
+                } else {
+                    count += 2;
+                    div = i;
+                }
+            } 
+        }
+
+        if(count == 4) {
+            return div;
+        } else {
+            return 0;
+        }
+    }
+};
