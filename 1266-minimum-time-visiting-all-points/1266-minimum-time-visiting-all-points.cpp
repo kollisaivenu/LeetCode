0f55da7f7b12b5c2554f13f62c48cffcb27bb6d7
kollisaivenu <38183134+kollisaivenu@users.noreply.github.com>
2026-01-11 18:12:13 -0700

    Time: 0 ms (100%), Space: 13.8 MB (26.76%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -3,7 +3,6 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int total = 0;
         for(int i=1;i<points.size();i++) {
-            //cout<<minDist(points[i-1][0], points[i-1][1], points[i][0], points[i][1])<<"\n";
             total += minDist(points[i-1][0], points[i-1][1], points[i][0], points[i][1]);
         }
 
