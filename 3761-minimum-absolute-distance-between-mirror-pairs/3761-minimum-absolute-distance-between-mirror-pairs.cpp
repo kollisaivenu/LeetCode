530908cc671711fc7384cbcc4fc178682ca1d881
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-04-16 21:07:04 -0600

    Time: 106 ms (75.74%), Space: 132 MB (57.35%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,66 +1,26 @@
 class Solution {
 public:
     int minMirrorPairDistance(vector<int>& nums) {
-        unordered_map<int, vector<int>>rev_index;
-        vector<int>rev;
-        for(int i=0;i<nums.size();i++) {
-            int rev_num = reverseInt(nums[i]);
-            rev.push_back(rev_num);
-            rev_index[nums[i]].push_back(i);
-        }
-        
-        int ans = INT_MAX;
-        for(int i=0; i<nums.size();i++) {
-            if(rev_index[rev[i]].size() == 0) {
-                continue;
+        auto reverseNum = [](int x) {
+            int y = 0;
+            while (x > 0) {
+                y = y * 10 + x % 10;
+                x /= 10;
             }
+            return y;
+        };
 
-            int index = getLargestSmall(i, rev_index[rev[i]]);
-
-            if (index == -1) {
-                if(i != rev_index[rev[i]][0]) {
-                    ans = min(ans, abs(i - rev_index[rev[i]][0]));
-                }
-            } else if(index == rev_index[rev[i]].size()-1) {
-                continue;
-            } else {
-                if(i != rev_index[rev[i]][index+1]) {
-                    ans = min(ans, abs(i - rev_index[rev[i]][index+1]));
-                }
+        int n = nums.size();
+        unordered_map<int, int> prev;
+        int ans = n + 1;
+        for (int i = 0; i < n; ++i) {
+            int x = nums[i];
+            if (prev.count(x)) {
+                ans = min(ans, i - prev[x]);
             }
-        }
-        if(ans == INT_MAX) {
-            return -1;
+            prev[reverseNum(x)] = i;
         }
 
-        return ans;
-    }
-
-    int reverseInt(int num) {
-        int n = 0;
-        while(num) {
-            int dig = num%10;
-            n = n*10+dig;
-            num = num/10;
-        }
-
-        return n;
-    }
-
-    int getLargestSmall(int index, vector<int>&indices) {
-        int low = 0, high = indices.size()-1;
-        int ans = -1;
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-
-            if(indices[mid] <= index) {
-                ans = mid;
-                low = mid+1;
-            } else {
-                high = mid-1;
-            }
-        }
-
-        return ans;
+        return ans == n + 1 ? -1 : ans;
     }
 };
