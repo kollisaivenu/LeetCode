ed93c11578835e0686a9c34870a4b911c008913f
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-17 00:11:46 -0600

    Time: 0 ms (100%), Space: 16.2 MB (97.13%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int>parent(n, 0);
+
+        for(int i=0;i<n;i++) {
+            parent[i] = i;
+        }
+
+        for(int i=0;i<edges.size();i++) {
+            if(find(edges[i][0], parent) != find(edges[i][1], parent)) {
+                un(edges[i][0], edges[i][1], parent);
+            }
+        }
+        int count = 0;
+
+        for(int i=0;i<n;i++) {
+            if(i == parent[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    int find(int a, vector<int>&parent) {
+        if(a == parent[a]) {
+            return a;
+        }
+
+        parent[a] = find(parent[a], parent);
+        return parent[a];
+    }
+
+    void un(int a, int b, vector<int>&parent) {
+        int parent_a = find(a, parent);
+        int parent_b = find(b, parent);
+        parent[parent_a] = parent_b;
+    }
+};
