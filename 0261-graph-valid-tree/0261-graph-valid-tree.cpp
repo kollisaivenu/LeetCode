ca9cfa40477d8eb207f45cd17289c38011c355c5
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-17 00:02:26 -0600

    Time: 0 ms (100%), Space: 16.2 MB (88.96%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int>parent(n, 0);
+
+        for(int i=0;i<n;i++) {
+            parent[i] = i;
+        }
+
+        for(int i=0;i<edges.size();i++) {
+            if(find(edges[i][0], parent) == find(edges[i][1], parent)) {
+                return false;
+            } else {
+                un(edges[i][0], edges[i][1], parent);
+            }
+        }
+
+        return edges.size() == n-1;
+    }
+
+    int find(int a, vector<int>&parent) {
+        if(a == parent[a]) {
+            return a;
+        }
+        return find(parent[a], parent);
+    }
+
+    void un(int a, int b, vector<int>&parent) {
+        int parent_a = find(a, parent);
+        int parent_b = find(b, parent);
+
+        parent[parent_a] = parent_b;
+    }
+};
