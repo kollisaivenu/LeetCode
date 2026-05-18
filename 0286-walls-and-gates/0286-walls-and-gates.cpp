87690cdc1d1cb9868636b8b30959f4685f1a38e6
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-18 00:01:36 -0600

    Time: 195 ms (5.06%), Space: 92.6 MB (5.22%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        queue<pair<pair<int, int>, int>>q;
+        int rows = rooms.size();
+        int cols = rooms[0].size();
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                if(rooms[i][j] == 0) {
+                    q.push(make_pair(make_pair(i, j), 0));
+                }
+            }
+        }
+
+        while(!q.empty()) {
+            pair<int, int>p = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+            int x = p.first;
+            int y = p.second;
+            if(rooms[x][y] > dist) {
+                rooms[x][y] = dist;
+            }   
+            
+            
+            if(x-1 >= 0 && rooms[x-1][y] > dist+1) {
+                q.push(make_pair(make_pair(x-1, y), dist+1));
+            }
+
+            if(y-1 >= 0 && rooms[x][y-1] > dist+1) {
+                q.push(make_pair(make_pair(x, y-1), dist+1));
+            }
+
+            if(x+1 < rows && rooms[x+1][y] > dist+1) {
+                q.push(make_pair(make_pair(x+1, y), dist+1));
+            }
+
+            if(y+1 < cols && rooms[x][y+1] > dist+1) {
+                q.push(make_pair(make_pair(x, y+1), dist+1));
+            }
+        }
+    }
+};
