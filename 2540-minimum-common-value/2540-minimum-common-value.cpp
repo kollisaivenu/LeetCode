71748d968c13e5cddecb4a1b51763bf6633a7b98
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-18 18:07:54 -0600

    Time: 84 ms (5.03%), Space: 84.6 MB (5.19%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-
         unordered_map<int, int>freq;
-
+        int mini = INT_MAX;
         for(int i=0;i<nums1.size();i++) {
             freq[nums1[i]]++;
         }
 
         for(int i=0;i<nums2.size();i++) {
             if(freq[nums2[i]] > 0) {
-                return nums2[i];
+                mini = min(nums2[i], mini);
             }
         }
-
-        return -1;
+        
+        if(mini == INT_MAX) {
+            return -1;
+        } else {
+            return mini;
+        }
+        
     }
 };
