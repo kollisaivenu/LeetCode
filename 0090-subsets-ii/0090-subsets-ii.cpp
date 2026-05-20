ea694b0a0243d5f7d1bea6c684797311d9182eaa
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-19 23:04:01 -0600

    Time: 3 ms (31.49%), Space: 15.1 MB (10.35%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -9,7 +9,7 @@ public:
         return ans;
     }
 
-    void subset(vector<vector<int>>&ans, set<vector<int>>&vis, vector<int>&nums, int i, vector<int>array) {
+    void subset(vector<vector<int>>&ans, set<vector<int>>&vis, vector<int>&nums, int i, vector<int>&array) {
         if(i == nums.size()) {
             if(!vis.contains(array)) {
                 ans.push_back(array);
@@ -20,6 +20,7 @@ public:
 
         subset(ans, vis, nums, i+1, array);
         array.push_back(nums[i]);
-        subset(ans, vis, nums, i+1, array);        
+        subset(ans, vis, nums, i+1, array);
+        array.pop_back();        
     }
 };
