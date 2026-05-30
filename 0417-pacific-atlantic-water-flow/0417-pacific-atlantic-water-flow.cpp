1f48bc096da5163daa2b38db71551d9795a70037
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-05-29 22:41:07 -0600

    Time: 7 ms (62.43%), Space: 22.6 MB (63.52%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -9,19 +9,23 @@ public:
         
         // pacific 
         for(int i=0;i<cols;i++) {
+            pacific[0][i] = true;
             dfs(0, i, heights, pacific);
         }
         
         for(int i=0;i<rows;i++) {
+            pacific[i][0] = true;
             dfs(i, 0, heights, pacific);
         }
 
         // atlantic
         for(int i=0;i<cols;i++) {
+            atlantic[rows-1][i] = true;
             dfs(rows-1, i, heights, atlantic);
         }
         
         for(int i=0;i<rows;i++) {
+            atlantic[i][cols-1] = true;
             dfs(i, cols-1, heights, atlantic);
         }
 
@@ -40,23 +44,26 @@ public:
     }
 
     void dfs(int x, int y, vector<vector<int>>& heights, vector<vector<bool>>&ocean) {
-        ocean[x][y] = true;
         int rows = heights.size();
         int cols = heights[0].size();
 
         if(x-1 >= 0 && heights[x-1][y] >= heights[x][y] && ocean[x-1][y] == false) {
+            ocean[x-1][y] = true;
             dfs(x-1, y, heights, ocean);
         }
 
         if(x+1 < rows && heights[x+1][y] >= heights[x][y] && ocean[x+1][y] == false) {
+            ocean[x+1][y] = true;
             dfs(x+1, y, heights, ocean);
         }
 
         if(y-1 >= 0 && heights[x][y-1] >= heights[x][y] && ocean[x][y-1] == false) {
+            ocean[x][y-1] = true;
             dfs(x, y-1, heights, ocean);
         }
 
         if(y+1 < cols && heights[x][y+1] >= heights[x][y] && ocean[x][y+1] == false) {
+            ocean[x][y+1] = true;
             dfs(x, y+1, heights, ocean);
         }
         
