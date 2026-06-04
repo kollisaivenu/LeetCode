274ea35c6092b4b6468b8de8ed5bd95a367c37bd
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-06-04 12:34:15 -0700

    Time: 7 ms (43.26%), Space: 94.8 MB (5.1%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
+    static bool cmp(pair<int, int>&a, pair<int, int>&b) {
+        return a.second < b.second;
+    }
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int ans = INT_MAX;
+        vector<pair<int, int>>land;
+        vector<pair<int, int>>water;
         for(int i=0;i<landStartTime.size();i++) {
-            for(int j=0;j<waterStartTime.size();j++) {
-                int time = max(landStartTime[i]+landDuration[i], waterStartTime[j]) + waterDuration[j];
-                ans = min(ans, time);
-            }
+            land.push_back(make_pair(landStartTime[i], landStartTime[i]+landDuration[i]));
         }
 
         for(int i=0;i<waterStartTime.size();i++) {
-            for(int j=0;j<landStartTime.size();j++) {
-                int time = max(waterStartTime[i]+waterDuration[i], landStartTime[j]) + landDuration[j];
-                ans = min(ans, time);
+            water.push_back(make_pair(waterStartTime[i], waterStartTime[i]+waterDuration[i]));
+        }
+
+        sort(land.begin(), land.end(), cmp);
+        sort(water.begin(), water.end(), cmp);
+
+        for(int i=0;i<waterStartTime.size();i++) {
+            if(land[0].second <= waterStartTime[i]) {
+                ans = min(ans, waterStartTime[i] + waterDuration[i]);
+            } else {
+                ans = min(ans, land[0].second + waterDuration[i]);
+            }
+        }
+
+        for(int i=0;i<landStartTime.size();i++) {
+            if(water[0].second <= landStartTime[i]) {
+                ans = min(ans, landStartTime[i] + landDuration[i]);
+            } else {
+                ans = min(ans, water[0].second + landDuration[i]);
             }
         }
 
         return ans;
-        
     }
 };
