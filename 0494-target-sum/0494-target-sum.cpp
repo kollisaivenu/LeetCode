5dc5f418290bd7f0fe569758b9a39385220fdab7
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-06-08 12:55:14 -0700

    Time: 263 ms (28.96%), Space: 62.3 MB (6.49%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return recurse(0, nums, 0, target);
+        map<pair<int, int>, int>dp;
+        return recurse(0, nums, 0, target, dp);
     }
 
-    int recurse(int i, vector<int>&nums, int curr_sum, int target) {
+    int recurse(int i, vector<int>&nums, int curr_sum, int target, map<pair<int, int>, int>&dp) {
         if(i == nums.size()) {
             if(curr_sum == target) {
                 return 1;
@@ -12,13 +13,13 @@ public:
                 return 0;
             }
         }
-
+        if(dp.contains({i, curr_sum})) {
+            return dp[{i, curr_sum}];
+        }
         int ans = 0;
-        ans += recurse(i+1, nums, curr_sum + nums[i], target);
-        ans += recurse(i+1, nums, curr_sum - nums[i], target);
-
+        ans += recurse(i+1, nums, curr_sum + nums[i], target, dp);
+        ans += recurse(i+1, nums, curr_sum - nums[i], target, dp);
+        dp[{i, curr_sum}] = ans;
         return ans;
-
-
     }
 };
