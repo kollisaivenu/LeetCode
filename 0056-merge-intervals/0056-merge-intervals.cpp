13c6c015e4bab112268ca2351094cb29052a3077
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-06-29 00:15:55 -0700

    Time: 7 ms (38.97%), Space: 23.9 MB (31.08%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&interval1, vector<int>&interval2) {
+        if(interval1[0] == interval2[0]) {
+            return interval1[1] < interval2[1];
+        }
+
+        return interval1[0] < interval2[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int start = intervals[0][0], end = intervals[0][1];
+        
+        for(int i=1;i<intervals.size();i++) {
+            if(intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            } else {
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        if(start != INT_MAX) {
+            ans.push_back({start, end});
+        }
+
+        return ans;
+    }
+};
