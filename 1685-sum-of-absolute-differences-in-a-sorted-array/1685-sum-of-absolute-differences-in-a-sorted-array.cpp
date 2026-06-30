fd7d00a65e3b87f517d049acb8e73aade9c8a300
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-06-29 19:22:56 -0700

    Time: 63 ms (94.03%), Space: 87.3 MB (92.04%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0;i<nums.size();i++) {
             right += nums[i];
         }
+        
         vector<int>ans(nums.size(), 0);
         for(int i=0;i<nums.size();i++) {
             right -= nums[i];
