8c53a9eca24b374bf71fb42b374c6e6de1ced88f
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-07-31 20:55:17 -0700

    Time: 0 ms (100%), Space: 10.7 MB (5.4%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>freq(26, 0);
+        for(char ch: word) {
+            freq[ch-'a']++;
+        }
+
+        sort(freq.rbegin(), freq.rend());
+        int assign = 0, ans = 0;
+        for(int val: freq) {
+            if(val == 0) break;
+            
+            if(assign < 8) {
+                ans += val;
+            } else if(assign < 16) {
+                ans += 2*val;
+            } else if(assign < 24) {
+                ans += 3*val;
+            } else {
+                ans += 4*val;
+            }
+
+            assign++;
+        }
+
+        return ans;
+    }
+};
