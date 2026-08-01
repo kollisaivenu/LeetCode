7a684b2b32e1ce1e7d9791df26d3948a8947c927
Saivenu Kolli <38183134+kollisaivenu@users.noreply.github.com>
2026-07-31 20:50:47 -0700

    Time: 87 ms (6.93%), Space: 27.2 MB (38.03%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        map<char, int>freq;
+        vector<pair<int, char>>pairs;
+        for(char ch: word) {
+            freq[ch]++;
+        }
+
+        for(auto [key, val]: freq) {
+            pairs.push_back(make_pair(val, key));
+        }
+
+        sort(pairs.begin(), pairs.end());
+        reverse(pairs.begin(), pairs.end());
+        int assign = 0, ans = 0;
+        for(auto [val, key]: pairs) {
+            if(assign < 8) {
+                ans += val;
+            } else if(assign < 16) {
+                ans += 2*val;
+            } else if(assign < 24) {
+                ans += 3*val;
+            } else {
+                ans += 4*val;
+            }
+
+            assign++;
+        }
+
+        return ans;
+        
+    }
+};
